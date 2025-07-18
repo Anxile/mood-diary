b44b492a5a85c1343cd79331bbf63deb04e8508c
Anxile <onebox.chen@outlook.com>
2025-07-18 17:52:49 -0400

    completed new post page

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l=vny3y1hp&+g^ohsdw+o5t!$4spw+%)ts$5z7lkh810t)ss@0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -117,13 +116,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR / 'assets'
+MEDIA_ROOT = BASE_DIR/ 'media'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),]
+    BASE_DIR/ 'static'
+]
 
-MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -30,4 +30,4 @@ urlpatterns = [
     path('users/', include('users.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
